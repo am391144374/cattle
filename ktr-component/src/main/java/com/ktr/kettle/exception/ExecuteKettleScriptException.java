4eda1f71ed7e1ace8047018f713051c1aef30f9c
lsj <am391144374@hotmail.com>
2021-03-10 11:29:38 +0800

    项目更名

--- ktr-component/src/main/java/com/ktr/kettle/exception/ExecuteKettleScriptException.java
+++
@@ -1,8 +0,0 @@
-package com.ktr.kettle.exception;
-
-public class ExecuteKettleScriptException extends RuntimeException {
-
-    public ExecuteKettleScriptException(String msg){
-        super(msg);
-    }
-}
